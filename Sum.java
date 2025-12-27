e00deba180f50df21c15f2041797d045871c7b79
Vatsal Nandedkar <Vatsalnandedkarkar@gmail.com>
2025-12-27 19:16:42 +0530

    add implement

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int sum = a + b;
+        int sum = a - b;
         System.out.println("Sum = " + sum);
     }
 }
